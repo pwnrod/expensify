ab56f4a70e9e42d3dfc6309aed48ba991b77dac5
Caylin James <caylinjames.dev@gmail.com>
2019-01-24 07:46:38 -0500

    Reducers

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 console.log(__dirname);
 
 module.exports = {
-    entry: './src/playground/redux-101.js',
+    entry: './src/playground/redux-expensify.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
